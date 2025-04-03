ef0fef0ecbe698fe8a418e8287dcbda8ea9ae5f2
William Souza <uiru42@gmail.com>
2025-04-02 22:16:36 -0300

    atributo --seed para popular o banco quando instalar a aplicação

--- docker-initdb.sh
+++ docker-initdb.sh
@@ -5,6 +5,6 @@ touch /var/www/html/database/database.sqlite
 chown www-data:www-data /var/www/html/database/database.sqlite
 chmod 664 /var/www/html/database/database.sqlite
 
-php artisan migrate
+php artisan migrate --seed
 
 exec "$@"
