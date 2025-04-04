4dc6c37bfef6412799136de491257217dce288b7
William Souza <uiru42@gmail.com>
2025-04-04 02:16:57 -0300

    ajuste de permissões e link para o swagger na home

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,6 +3,12 @@
 # Inicializa o supervisor em segundo plano
 /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisor.conf &
 
+# Adiciona permissões necessárias
+chown -R www-data:www-data /var/www/html/storage
+chmod -R 775 /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/bootstrap/cache
+
 # Aguarda o PostgreSQL ficar disponível
 while ! nc -z db 5432; do
   echo "Aguardando o PostgreSQL..."
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,6 +28,16 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
         <div class="container">
             <a class="navbar-brand" href="/">Teste Técnico</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/api/documentation">Swagger</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 
